d5b56b87a7a4b833489bd1d4a7db9d826900dbca
MattH <mhoskins@nurock.com>
2018-11-08 08:18:47 -0500

    example JSON

---
+++ json_example.py
@@ -0,0 +1,17 @@
+import json
+
+#imagine this is a custom string gathered from a DB
+STRING_JSON_FROM_DB_EXAMPLE = "{\
+	'customField1': 'Unit Number',\
+	'customField2': 'Bedroom Count',\
+	'customField3': 'Residents'\
+}"
+
+print(type(STRING_JSON_FROM_DB_EXAMPLE))
+
+example = eval(STRING_JSON_FROM_DB_EXAMPLE) 	 # one line for conversion to dict
+
+print(type(example))
+
+print(example.get('customField1'))
+
